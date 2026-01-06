ec2d33be4aa2ac4422b8b2181aab2829feba9a83
devnyoni <devnyoni@gmail.com>
2026-01-06 10:00:44 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "raheem-xmd-deployer",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.0",
+    "express": "^4.19.0"
+  }
+}
